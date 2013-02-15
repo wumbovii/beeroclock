b1f47dac680dcc8d8e09fda2cab557514b44fe04
Christopher Coleman <Christopher@Christopher-Colemans-MacBook.local>
2013-02-15 11:15:19 -0800

    adding base app

---
+++ beeroclock/beeroclock/BeerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BeerAppDelegate.h
+//  beeroclock
+//
+//  Created by Christopher Coleman on 2/15/13.
+//  Copyright (c) 2013 Christopher Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BeerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ beeroclock/beeroclockTests/beeroclockTests.m
@@ -0,0 +1,32 @@
+//
+//  beeroclockTests.m
+//  beeroclockTests
+//
+//  Created by Christopher Coleman on 2/15/13.
+//  Copyright (c) 2013 Christopher Coleman. All rights reserved.
+//
+
+#import "beeroclockTests.h"
+
+@implementation beeroclockTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in beeroclockTests");
+}
+
+@end
